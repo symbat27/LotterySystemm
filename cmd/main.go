40e22b8ab3fba74c4a1e54d9fc72e0a95712f07d
GulnazNurseit <gulnaznurseit1227@gmail.com>
2026-02-08 19:09:34 +0500

    jkh

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"LotterySystem/internal/handlers"
+	"LotterySystem/internal/services"
+	"LotterySystem/internal/storage"
+	"log"
+	"net/http"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	userRepo := storage.NewUserRepository()
+	drawRepo := storage.NewDrawRepository()
+	ticketRepo := storage.NewTicketRepository()
+	prizeRepo := storage.NewPrizeRepository()
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
-	}
+	service := services.NewLotteryService(
+		userRepo,
+		drawRepo,
+		ticketRepo,
+		prizeRepo,
+	)
+
+	userHandler := handlers.NewUserHandler(service)
+	ticketHandler := handlers.NewTicketHandler(service)
+	adminHandler := handlers.NewAdminHandler(service)
+
+	mux := http.NewServeMux()
+	userHandler.Register(mux)
+	ticketHandler.Register(mux)
+	adminHandler.Register(mux)
+
+	fs := http.FileServer(http.Dir("./internal/frontend"))
+	mux.Handle("/", fs)
+
+	log.Println("Lottery Imitation System")
+	log.Println("User panel:  http://localhost:8080/index.html")
+	log.Println("Admin panel: http://localhost:8080/admin.html")
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
